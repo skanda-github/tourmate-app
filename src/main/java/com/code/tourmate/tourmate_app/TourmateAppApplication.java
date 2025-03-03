1215eee23af4b3446fb2f2c3fe0fb4247eccc445
Skanda <std@kmd.dk>
2025-03-03 16:40:34 +0530

    Initial Spring Boot setup

---
+++ src/main/java/com/code/tourmate/tourmate_app/TourmateAppApplication.java
@@ -0,0 +1,13 @@
+package com.code.tourmate.tourmate_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TourmateAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TourmateAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/code/tourmate/tourmate_app/TourmateAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code.tourmate.tourmate_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourmateAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
